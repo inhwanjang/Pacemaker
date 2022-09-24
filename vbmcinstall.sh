310c3ed93feef5da109c77c09bde325da0ba5154
inhwanjang <114274269+inhwanjang@users.noreply.github.com>
2022-09-24 20:42:27 +0900

    Create vbmcinstall.sh

---
+++ vbmcinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+yum -y install centos-release-openstack-train && yum -y install python2-virtualbmc
+
+touch /etc/systemd/system/vbmcd.service
+echo "[Install]
+WantedBy = multi-user.target
+
+[Service]
+BlockIOAccounting = True
+CPUAccounting = True
+ExecReload = /bin/kill -HUP $MAINPID
+ExecStart = /usr/bin/vbmcd --foreground
+Group = root
+MemoryAccounting = True
+PrivateDevices = False
+PrivateNetwork = False
+PrivateTmp = False
+PrivateUsers = False
+Restart = on-failure
+RestartSec = 2
+Slice = vbmc.slice
+TasksAccounting = True
+TimeoutSec = 120
+Type = simple
+User = root
+
+[Unit]
+After = libvirtd.service
+After = syslog.target
+After = network.target
+Description = vbmc service
+"
+>> /etc/systemd/system/vbmcd.service
+
+systemctl daemon-reload && systemctl start vbmcd 
+systemctl enable vbmcd
+systemctl status vbmcd
+
+## process running error: rm -rf /root/.vbmc/master.pid
