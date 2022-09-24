46b31aafbd76b1c6715d354117fb0c2662801990
inhwanjang <114274269+inhwanjang@users.noreply.github.com>
2022-09-24 23:34:09 +0900

    Update node_create.sh

--- node_create.sh
+++ node_create.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+echo " Two node cluster ..."
+echo "10seconds...if you won't that you pause ctrl + c"
+sleep 10;
 
+read -p "node name : " node
+virt-install --name $node --ram 2048 \
+--disk path=/VM/$node.img,size=15 --vcpus 1 \
+--os-type linux --network bridge=br0 --graphics none \
+--console pty,target_type=serial --location \
+/iso/CentOS-7-x86_64-DVD-2009.iso \
+--extra-args 'console=ttyS0,115200n8 serial'
+
+read -p "node name: " node
+virt-install --name $node --ram 2048 \
+--disk path=/VM/$node.img,size=15 --vcpus 1 \
+--os-type linux --network bridge=br0 --graphics none \
+--console pty,target_type=serial --location \
+/iso/CentOS-7-x86_64-DVD-2009.iso \
+--extra-args 'console=ttyS0,115200n8 serial'
