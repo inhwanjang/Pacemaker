3c4674c5b3a4ff4d2c09dd2504e94dee3afc599a
inhwanjang <114274269+inhwanjang@users.noreply.github.com>
2022-09-24 23:31:49 +0900

    Update kvm_install.sh

--- kvm_install.sh
+++ kvm_install.sh
@@ -25,9 +25,24 @@ yum -y install qemu-kvm libvirt virt-install bridge-utils virt-manager wget
 systemctl start libvirtd && systemctl enable libvirtd
 mkdir /VM
 mkdir /iso
+
+echo "This is two node cluster configuration"
+read -p "continue ? : " answer
+if [ $answer == yes ];then
+bash ./nodecreate.sh
+
+fi
 wget -P /iso/ http://mirror.navercorp.com/centos/7.9.2009/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso 
 ##size=GB / ram = MB
 read -p "node name : " node
+virt-install --name $node --ram 2048 \
+--disk path=/VM/$node.img,size=15 --vcpus 1 \
+--os-type linux --network bridge=br0 --graphics none \
+--console pty,target_type=serial --location \
+/iso/CentOS-7-x86_64-DVD-2009.iso \
+--extra-args 'console=ttyS0,115200n8 serial'
+
+read -p "node name: " node
 virt-install --name $node --ram 2048 \
 --disk path=/VM/$node.img,size=15 --vcpus 1 \
 --os-type linux --network bridge=br0 --graphics none \
