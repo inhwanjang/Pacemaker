86559e5d2e188dc1c83925c1fac08799ac8423b3
inhwanjang <114274269+inhwanjang@users.noreply.github.com>
2022-09-25 00:13:24 +0900

    Update node_create.sh

--- kvm/node_create.sh
+++ kvm/node_create.sh
@@ -18,3 +18,11 @@ virt-install --name $node --ram 2048 \
 --console pty,target_type=serial --location \
 /iso/CentOS-7-x86_64-DVD-2009.iso \
 --extra-args 'console=ttyS0,115200n8 serial'
+
+read -p "node name: " node
+virt-install --name $node --ram 2048 \
+--disk path=/VM/$node.img,size=15 --vcpus 1 \
+--os-type linux --network bridge=br0 --graphics none \
+--console pty,target_type=serial --location \
+/iso/CentOS-7-x86_64-DVD-2009.iso \
+--extra-args 'console=ttyS0,115200n8 serial'
