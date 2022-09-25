7c62fe47896bfaedf05872edb890b819b475505a
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-25 16:33:40 +0900

    Update install.sh

--- iscsi/install.sh
+++ iscsi/install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+yum -y install targetcli
+
+targetcli create dev=/dev/sdb1 name=sdb1
+
+
+echo "cd /backstores/block && create dev=/dev/sdb1 name=sdb1" | targetcli
+
+echo "cd /iscsi && create wwn=iqn.2022-09.com.example:servers" | targetcli
+
+echo "cd /iscsi/iqn.2022-09.com.example:servers/tpgl/acls '&&' create wwn=iqn.2022-09.com.example:ha1.hb.com '&&' create wwn=iqn.2022-09.com.example:ha2.hb.com" | targetcli
+
+
+
+yum install iscsi-initiator-utils.x86_64
 
