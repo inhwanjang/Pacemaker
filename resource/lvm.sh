691211cb3c494fce40919fd0680635df847f513d
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-25 20:38:59 +0900

    Update lvm.sh

--- resource/lvm.sh
+++ resource/lvm.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "LVM and Filesystem resource configuration"
+read -p "vgname: " vg
+read -p "lvname: " lv
+read -p "LVM resource name: " resource1
+read -p "group name: " group
+read -p "Filesystem resource name: " resource2
+read -p "directory path name: " directory
+read -p "filesystem : " filesytem
+
+pcs resource create $resource1 LVM volgrpname=$vg exclusive=true --group $group
+
+pcs resource create $resource2 Filesystem device="/dev/$vg/$lv" directory="$directory" fstype="$filesytem" --group $group
