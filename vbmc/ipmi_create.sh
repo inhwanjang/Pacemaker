3d687b6ffbfc81a22e7c2282119c9243f01f27e2
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-25 14:31:13 +0900

    Update ipmi_create.sh

--- vbmc/ipmi_create.sh
+++ vbmc/ipmi_create.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+ssh-keygen -t rsa
+read -p "kvm ip : " ip
+read -p "kvm hostname : " host
+echo "$ip $host " >> /etc/hosts
+
+ssh-copy-id root@$host
+
 
 read -p "username : " username
 read -p "password : " password
@@ -9,8 +16,9 @@ read -p "host2 : " host2
 read -p "host2's port2 : " port2
 
 
-vbmc add --username $username --password $password --port $port1 --libvirt-uri qemu:///system $host1
-vbmc add --username $username --password $password --port $port2 --libvirt-uri qemu:///system $host2
+
+vbmc add --username $username --password $password --port $port1 --libvirt-uri qemu+ssh://root@$host/system $host1
+vbmc add --username $username --password $password --port $port2 --libvirt-uri qemu+ssh://root@$host/system $host2
 
 vbmc start $host1
 vbmc start $host2
