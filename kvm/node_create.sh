6ed2e0d34bc2a38aad22178f8eb3bc24abc16619
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-26 20:15:10 +0900

    Update node_create.sh

--- kvm/node_create.sh
+++ kvm/node_create.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "Virtualmachine create"
-echo "You must answer y Y or n N"
 
+myfunction(){
+read -p "clone ? [y/n] : " answer
 case $answer in
-  y,Y)
-  n,N)
+  [y]* ) read -p "how many clone? [number]" clone;;
+  [n]* ) echo "only one virtual machine create";;
+  * ) echo " only y or n " &&  myfunction;;
 esac
+}
+myfunction
+
 read -p "node name : " node
 virt-install --name $node --ram 2048 \
 --disk path=/VM/$node.img,size=15 --vcpus 1 \
@@ -14,18 +19,4 @@ virt-install --name $node --ram 2048 \
 /iso/CentOS-7-x86_64-DVD-2009.iso \
 --extra-args 'console=ttyS0,115200n8 serial'
 
-read -p "node name: " node
-virt-install --name $node --ram 2048 \
---disk path=/VM/$node.img,size=15 --vcpus 1 \
---os-type linux --network bridge=br0 --graphics none \
---console pty,target_type=serial --location \
-/iso/CentOS-7-x86_64-DVD-2009.iso \
---extra-args 'console=ttyS0,115200n8 serial'
-
-read -p "node name: " node
-virt-install --name $node --ram 2048 \
---disk path=/VM/$node.img,size=15 --vcpus 1 \
---os-type linux --network bridge=br0 --graphics none \
---console pty,target_type=serial --location \
-/iso/CentOS-7-x86_64-DVD-2009.iso \
---extra-args 'console=ttyS0,115200n8 serial'
+virt-clone --original $node --name $node-2 --file /VM/$node-2.img 
