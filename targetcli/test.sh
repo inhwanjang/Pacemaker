c03d303abe921c1c8efc123bff589ef0ea91aea6
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-26 12:48:43 +0900

    Update test.sh

--- targetcli/test.sh
+++ targetcli/test.sh
@@ -1,21 +1,21 @@
-yum install targetcli
-targetcli
-cd /backstores/block
-create dev=/dev/sdb1 name=sdb1
-cd /iscsi
-create wwn=iqn.2022-09.com.example:servers
-cd iqn.2022-09.com.example:servers/tpg1/acls
-create wwn=iqn.2022-09.com.example:ha1.hb.com
-create wwn=iqn.2022-09.com.example:ha2.hb.com
+yum -y install targetcli
 
-cd /iscsi/iqn.2022-08.com.example:servers/tpgl/luns
-create /backstores/block/sdb1
-exit
+
+
+echo "cd /backstores/block" | targetcli
+echo "create dev=/dev/vda1 name=vda1" | targetcli
+echo "cd /iscsi " | targetcli
+echo "create wwn=iqn.2022-09.com.example:servers" | targetcli
+
+echo "cd /iscsi/iqn.2022-09.com.example:servers/tpg1/acls" | targetcli
+echo "create wwn=iqn.2022-09.com.example:ha1.hb" | targetcli 
+echo "create wwn=iqn.2022-09.com.example:ha2.hb" | targetcli
+echo "cd /iscsi/iqn.2022-08.com.example:servers/tpgl/luns " | targetcli
+echo "create /backstores/block/vda1" | targetcli
+
+
 firewall-cmd --permanent --add-port=3260/tcp && firewall-cmd --reload
 systemctl start target && systemctl enable target
 
-ha1 , ha2 )
-
-yum -y install iscsi-initiator-utils
 
 
