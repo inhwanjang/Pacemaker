123f143b7a7316118e67156d63fed3382c0aece4
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-26 12:47:14 +0900

    Update install.sh

--- iscsi/install.sh
+++ iscsi/install.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-yum -y install targetcli
-
-
-
-echo "cd /backstores/block" | targetcli
-echo "create dev=/dev/vda1 name=vda1" | targetcli
-echo "cd /iscsi " | targetcli
-echo "create wwn=iqn.2022-09.com.example:servers" | targetcli
-
-echo "cd /iscsi/iqn.2022-09.com.example:servers/tpg1/acls " | targetcli
-echo "create wwn=iqn.2022-09.com.example:ha1.hb.com " | targetcli 
-echo "create wwn=iqn.2022-09.com.example:ha2.hb.com" | targetcli
-
-
-
-
+iscsi 연결 명령어
