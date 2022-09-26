307cc89a7544a2703fcdc173cff2d801e88a4166
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-26 20:40:55 +0900

    Update clone.sh

--- kvm/clone.sh
+++ kvm/clone.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+myfunction(){
+read -p "clone ? [y/n] : " answer
+case $answer in
+  [y]* ) read -p "how many clone? [number]" clone ;;
+         
+  [n]* ) echo "Bye" 
+  * ) echo " only y or n " &&  myfunction;;
+esac
+}
+myfunction
+
+
+
+for var in {1..$number}
+do
+virt-clone --original $node --name $node-2 --file /VM/$node-$var.img
+done
+
+
+read -p "number: " number
+for var in {1..`echo "$number"`}
+do
+echo "fuck you $var"
+done
