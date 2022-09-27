c7f7b9b28eb0139269dc0c2cb0d76c3bd2f1fec1
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-27 23:10:59 +0900

    Update fence_resource.sh

--- fence/fence_resource.sh
+++ fence/fence_resource.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pcs stonith create fence_a fence_ipmilan pcmk_host_list="ha1.hb.com" ipaddr=192.168.0.50 ipport=6230 login='admin' passwd='dlatl00' lanplus=on auth=password delay=15 power_wait=20
-pcs stonith create fence_b fence_ipmilan pcmk_host_list="ha2.hb.com" ipaddr=192.168.0.50 ipport=6231 login='admin' passwd='dlatl00' lanplus=on auth=password power_wait=20
+pcs stonith create fence_a fence_ipmilan pcmk_host_list="ha1.hb" ipaddr=192.168.0.119 ipport=6230 login='admin' passwd='dlatl00' lanplus=on auth=password delay=15 power_wait=20
+pcs stonith create fence_b fence_ipmilan pcmk_host_list="ha2.hb" ipaddr=192.168.0.119 ipport=6231 login='admin' passwd='dlatl00' lanplus=on auth=password power_wait=20
