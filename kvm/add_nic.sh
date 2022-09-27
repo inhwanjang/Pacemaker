6c53ea1bd459809232b8273cc448b1d3d8d4837d
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-27 11:36:26 +0900

    Create add_nic.sh

--- kvm/add_nic.sh
+++ kvm/add_nic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=cutecameron2&logNo=220591722501 참고하여 정리 
+<disk type='file' device='disk'>
+      <driver name='qemu' type='qcow2'/>
+      <source file='/VM/storage2.img'/>
+      <target dev='vda' bus='virtio'/>
+</disk>
