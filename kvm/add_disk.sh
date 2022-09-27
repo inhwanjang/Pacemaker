a2f0440e449fabc3e7a55d9db05bcef200c03a4c
InhwanJang <114274269+inhwanjang@users.noreply.github.com>
2022-09-27 22:27:40 +0900

    Update add_disk.sh

--- kvm/add_disk.sh
+++ kvm/add_disk.sh
@@ -5,6 +5,7 @@ qemu-img create -f qcow2 /VM/storage2.img 5G
 virsh edit storage
 
 <disk type='file' device='disk'>
-<driver name='qemu' type='qcow2' cache='none'/>
-<source file='/VM/storage2.img'/>
-<target dev='
+   <driver name='qemu' type='qcow2' cache='none'/>
+   <source file='/VM/storage2.img'/>
+   <target dev='vda' bus='virtio'/>
+</disk>
